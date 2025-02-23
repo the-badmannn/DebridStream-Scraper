14fbdc86677637d22c9f9871b8dd50b4cf40a2b0
codebutter-bit <codebutter20@gmail.com>
2025-02-23 11:28:36 -0500

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,9 @@
+[
+  {
+    "name": "Bitlord",
+    "url": "https://bitlordsearch.com/search?q=%s",
+    "type": "html",
+    "magnetTargetKey": ".search-result a[href^='magnet']|href",
+    "itemType": "magnet"
+  }
+]
