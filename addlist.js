87a1d7bcd33e753ce6eeb2ad1e060bc9e5c189d2
codebutter-bit <codebutter20@gmail.com>
2025-03-03 08:24:24 -0500

    Update addlist.js

--- addlist.js
+++ addlist.js
@@ -2,32 +2,33 @@ const addonList = [
   {
     title: "Ultra-Fast",
     logo: "./images/ff.png",
-    description: "PirateBay + BitSearch + Torrentio + MediaFusion",
+    description: "Torrentio + MediaFusion",
     link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/addons-en.json",
   },
-  {
-    title: "Torr-Med",
-    logo: "./images/candy-corn.png",
-    description: "Torrentio + MediaFusion",
-    link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/all-imdb-scrapers.json",
-  },
+  // {
+  //   title: "Torr-Med",
+  //   logo: "./images/candy-corn.png",
+  //   description: "Torrentio + MediaFusion",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/all-imdb-scrapers.json",
+  // },
   {
     title: "BigFatty",
     logo: "./images/burger.png",
-    description: "A lot of scrapers (could be slow on under powered devices)",
+    description: "A lot of scrapers & a lot more sources!",
     link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/all-in-one.json",
-  },
-  {
-    title: "Old-Timers",
-    logo: "./images/grandfather.png",
-    description:
-      "PirateBay + YTS + CloudTorrent + Lime-Torrents + Torrents-CSV + Bit-Search",
-    link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/en-scrapers.json",
-  },
-  {
-    title: "Just Torrentio",
-    logo: "./images/ok.png",
-    description: "Just Torrentio",
-    link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/torentio-en.json",
-  },
+  }
+  // ,
+  // {
+  //   title: "Old-Timers",
+  //   logo: "./images/grandfather.png",
+  //   description:
+  //     "PirateBay + YTS + CloudTorrent + Lime-Torrents + Torrents-CSV + Bit-Search",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/en-scrapers.json",
+  // },
+  // {
+  //   title: "Just Torrentio",
+  //   logo: "./images/ok.png",
+  //   description: "Just Torrentio",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/torentio-en.json",
+  // },
 ];
